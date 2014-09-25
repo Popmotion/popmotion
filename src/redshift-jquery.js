bfee836d40133fdfc02784738f5d015a9363b604
Matt Perry <mattgperry@gmail.com>
2014-09-25 16:44:41 +0100

    getting play to work as a jquery plugin

--- src/redshift-jquery.js
+++ src/redshift-jquery.js
@@ -3,28 +3,22 @@
 	var KEY = {
 			ELEMENT: '_jQueryElement',
 			RS: 'redshift'
-		},
-		launch = function (run, $e) {
-			$e.each(function () {
-				var $this = $(this);
-					
-				if (!$this.redshift) {
-					$this.redshift = Redshift.get();
-					$this.redshift.data(KEY.ELEMENT, $this);
-				}
-				
-				$this.redshift[run](arguments[2], arguments[3], arguments[4], arguments[5], arguments[6]);
-			});
 		};
 	
-	$.fn.play = function() {
-		launch('play', this, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
-		
-		return this;
-	};
-	
-	$.fn.track = function () {
-		launch('track', this, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
+	$.fn.play = function(actions) {
+	    
+	    this.each(function () {
+    	    var $this = $(this),
+    	        thisRS = $this.data(KEY.RS);
+    	    
+    	    if (!thisRS) {
+        	    thisRS = Redshift.get();
+        	    thisRS.data(KEY.ELEMENT, $this);
+        	    $this.data(KEY.RS, thisRS);
+    	    }
+
+    	    thisRS.play(actions);
+	    });
 		
 		return this;
 	};
