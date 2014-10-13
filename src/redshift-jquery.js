d2d1e1a6ed7b57a04ea691abbdc52141c5f3c54d
Matt Perry <Matt@Winterfell.local>
2014-10-13 10:51:11 +0100

    refactoring jquery plugin

--- src/redshift-jquery.js
+++ src/redshift-jquery.js
@@ -1,7 +1,10 @@
 /*
     Redshift jQuery plugin
     
-    TODO: Refactor to remove all this repeated code. Jeez it's a mess in here.
+    Provides access to .play, .move and .track properties on an jQuery object.
+    Uses that jQuery object to store a unqiue Redshift instance.
+    
+    .redshift() method used for other Redshift functions, ie $('#element').redshift('stop')
 */
 
 require('./redshift.js');
@@ -11,78 +14,43 @@ require('./redshift.js');
 	var KEY = {
 			ELEMENT: '_jQueryElement',
 			RS: 'redshift'
+		},
+		execute = function ($element, action, arg1, arg2, arg3) {
+			$element.each(function () {
+				var $this = $(this),
+					thisRedshift = $this.data(KEY.RS);
+				
+				// If we haven't stored a Redshift instance on this element, make one
+				if (!thisRedshift) {
+					thisRedshift = Redshift.get();
+					thisRedshift.data(KEY.ELEMENT, $this);
+					$this.data(KEY.RS, thisRedshift);
+				}
+				thisRedshift[action](arg1, arg2, arg3);
+			});
 		};
-	
-	$.fn.play = function(actions, overrides) {
-	    
-	    this.each(function () {
-    	    var $this = $(this),
-    	        thisRS = $this.data(KEY.RS);
-    	    
-    	    if (!thisRS) {
-        	    thisRS = Redshift.get();
-        	    thisRS.data(KEY.ELEMENT, $this);
-        	    $this.data(KEY.RS, thisRS);
-    	    }
-
-    	    thisRS.play(actions, overrides);
-	    });
+		
+	$.fn.play = function () {
+		execute(this, 'play', arguments[0], arguments[1]);
 		
 		return this;
 	};
 	
-	$.fn.speed = function(actions) {
-	    
-	    this.each(function () {
-    	    var $this = $(this),
-    	        thisRS = $this.data(KEY.RS);
-    	    
-    	    if (!thisRS) {
-        	    thisRS = Redshift.get();
-        	    thisRS.data(KEY.ELEMENT, $this);
-        	    $this.data(KEY.RS, thisRS);
-    	    }
+	$.fn.move = function () {
+		execute(this, 'move', arguments[0], arguments[1]);
 
-    	    thisRS.speed(actions);
-	    });
+		return this;
+	};
+	
+	$.fn.track = function () {
+		execute(this, 'track', arguments[0], arguments[1]);
 		
 		return this;
 	};
 	
-	$.fn.track = function(actions, e) {
-	    
-	    this.each(function () {
-    	    var $this = $(this),
-    	        thisRS = $this.data(KEY.RS);
-    	    
-    	    if (!thisRS) {
-        	    thisRS = Redshift.get();
-        	    thisRS.data(KEY.ELEMENT, $this);
-        	    $this.data(KEY.RS, thisRS);
-    	    }
-
-    	    thisRS.track(actions, e);
-	    });
+	$.fn.redshift = function (action) {
+		execute(this, action, arguments[2], arguments[3]);
 		
 		return this;
 	};
-	
-	$.fn.redshift = function(action) {
-	    
-	    this.each(function () {
-    	    var $this = $(this),
-    	        thisRS = $this.data(KEY.RS);
-    	    
-    	    if (!thisRS) {
-        	    thisRS = Redshift.get();
-        	    thisRS.data(KEY.ELEMENT, $this);
-        	    $this.data(KEY.RS, thisRS);
-    	    }
-
-    	    thisRS[action]();
-	    });
-		
-		return this;
-	};
-	
 })(jQuery);
