066fbbd023a2631a0dd8646098d0075e28a7b5c4
Matt Perry <Matt@Winterfell.local>
2015-02-10 22:03:28 +0000

    added limiter

--- src/action/processor.js
+++ src/action/processor.js
@@ -91,98 +91,6 @@ Process.prototype = {
         }
     
         action.framestamp = framestamp;
-    
-    /*
-        var output = {},
-            props = action.props.store,
-            data = action.data.store,
-            values = action.values.store,
-            value,
-            rubix = props.rubix,
-            hasLinked = false,
-            hasChanged = false;
-
-        // Fire onStart if firstFrame
-        if (action.firstFrame) {
-            if (props.onStart) {
-                props.onStart.call(props.scope, data);
-            }
-
-            action.firstFrame = false;
-        }
-        
-        // Update associated Input
-        if (props.input) {
-	        output.input = props.input.onFrame(framestamp);
-        }
-        
-        // Update progress
-        action.progress = rubix.calcProgress(action, props, values, framestamp, frameDuration);
-
-        // Calculate new values
-        for (var key in values) {
-            if (values.hasOwnProperty(key)) {
-                value = values[key].store;
-
-                // Calculate new value if not currently linked
-                if (!value.link) {
-                    output[key] = rubix.easeValue(key, value, action, frameDuration);
-    
-                    // Round
-                    if (value.round) {
-                        output[key] = Math.round(output[key]);
-                    }
-    
-                    // Check if has changed
-                    if (value.current != output[key]) {
-                        hasChanged = true;
-                        value.current = output[key];
-                    }
-                } else {
-                    hasLinked = true;
-                }
-            }
-        } // end value calculations
-        
-        // If we have values that are linking in to another
-        if (hasLinked) {
-            for (key in values) {
-                value = values[key].store;
-                if (value.link && values[value.link]) {
-                    value.current = output[key] = this.resolveMaps(values[value.link].store.current, value.mapLink, value.mapTo);
-                }
-            }
-        }
-
-        // Calculate new x and y if angle and distance present
-        output = this.angleAndDistance(values, output);
-
-        // Fire onFrame callback
-        if (props.onFrame) {
-            props.onFrame.call(props.scope, output, data);
-        }
-
-        // Fire onChange callback
-        if (hasChanged && props.onChange) {
-            props.onChange.call(props.scope, output, data);
-        }
-        
-        // Fire onEnd and deactivate if at end
-        if (rubix.hasEnded(action, hasChanged)) {
-            action.isActive(false);
-
-            if (props.onEnd) {
-                props.onEnd.call(props.scope, output, data);
-            }
-            
-            // Check if action is still inactive before checking next action
-            if (!action.isActive()) {
-                action.next();
-            }
-        }
-        
-        // Update Action framestamp
-        action.framestamp = framestamp;*/
     },
     
     /*
@@ -213,7 +121,7 @@ Process.prototype = {
 	    @param [object]: Action origin point
 	    @param [object]: Current output
 	    @return [object]: Output with updated x and y
-    */
+    
     angleAndDistance: function (values, output) {
 	    var point = {};
 
@@ -224,7 +132,7 @@ Process.prototype = {
 	    }
 
 	    return output;
-    }
+    }*/
 };
 
 module.exports = new Process();
