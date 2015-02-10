844f7e5427c0cf25856c316f5da1c0d44edc5313
MattPerry8 <matt.perry@photobox.com>
2015-02-10 12:45:32 +0000

    adding array handling to copy/merge and updating value.current

--- src/action/processor.js
+++ src/action/processor.js
@@ -75,7 +75,7 @@ Process.prototype = {
             for (key in values) {
                 value = values[key].store;
                 if (value.link && values[value.link]) {
-                    output[key] = this.resolveMaps(values[value.link].store.current, value.mapLink, value.mapTo);
+                    value.current = output[key] = this.resolveMaps(values[value.link].store.current, value.mapLink, value.mapTo);
                 }
             }
         }
