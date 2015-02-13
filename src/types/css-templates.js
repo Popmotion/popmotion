27e31acd74836b7a53f2db22f9a66467eaf5c333
Matt Perry <Matt@Winterfell.local>
2015-02-13 00:33:48 +0000

    css templates

---
+++ src/types/css-templates.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var methods = ['translate', 'scale', 'skew', 'rotate', 'matrix'],
+    dimensions = ['', '3d', 'X', 'Y', 'Z'],
+    templates = {
+        color: function (r, g, b, a) {
+            return 'rgba(' + r + ','+ g + ',' + b + ',' + a + ')';
+        }
+        extend: function (key, method) {
+            this[key] = method;
+        }
+    };
+    
+(function () {
+    var methodsLength = methods.length,
+        dimensionsLength = dimensions.length,
+        key = '';
+        
+    for (var i = 0; i < methodsLength; i++) {
+        for (var j = 0; j < dimensionsLength; j++) {
+            key = methods[i] + dimensions[j];
+            templates.extend(key, function () {
+                var prop = key + '(',
+                    argsLength = arguments.length,
+                    lastKey = argsLength - 1;
+
+                for (var i = 0; i < argsLength; i++) {
+                    prop += arguments[i];
+                    if (i !== lastKey) {
+                        prop += ',';
+                    }
+                }
+
+                return prop + ')';
+            });
+        }
+    }
+})();
+    
+
+
+module.exports = templates;
