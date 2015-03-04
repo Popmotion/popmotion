aac2a09c8fd92471216aa4d517c352e12bb655f3
MattPerry8 <matt.perry@photobox.com>
2015-03-04 11:23:47 +0000

    removing deleteme ref files

--- _deleteme/css/dictionary.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-    colors: ['Red', 'Green', 'Blue', 'Alpha'],
-    positions: ['X', 'Y', 'Z'],
-    dimensions: ['Top', 'Right', 'Bottom', 'Left'],
-    shadow: ['X', 'Y', 'Radius', 'Spread', 'Color']
-};
--- _deleteme/css_/unit-dictionary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-    
-    dimensions: ['Top', 'Right', 'Bottom', 'Left'],
-    
-    shadow: ['X', 'Y', 'Radius', 'Spread', 'Color']
-    
-};
