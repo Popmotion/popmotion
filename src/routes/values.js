4360794d80e1acd73bc7d5c724068e12ab10650d
MattPerry8 <matt.perry@photobox.com>
2015-03-05 11:20:16 +0000

    dynamically generating value callback firers

--- src/routes/values.js
+++ src/routes/values.js
@@ -5,32 +5,37 @@
 */
 "use strict";
 
-var fireCallback = function (name, bucket, action, values, props, data) {
+var generateCallbackFire = function (name) {
+        return function () {
+            fireCallback.apply(this, [].slice.call(arguments).unshift(name));
+        }
+    },
+    
+    fireCallback = function (name, bucket, action, values, props, data) {
         if (props[name]) {
             props[name].call(props.scope, bucket, data);
         }
-    };
+    },
+    
+    callbacks = ['onChange', 'onFrame', 'onEnd'],
+    
+    valuesRoute = {
+        makeDefault: true,
+        
+        name: 'values',
+        
+        preprocess: function (key, value, action, props) {
+            action.setValue(key, value, props);
+        }
+    },
+    
+    i = 0,
+    
+    callbackName = '';
+    
+for (; i < callbacks.length; i++) {
+    callbackName = callbacks[i];
+    valuesRoute[callbackName] = generateCallbackFire(callbackName);
+}
 
-module.exports = {
-    
-    makeDefault: true,
-    
-    name: 'values',
-    
-    preprocess: function (key, value, action, props) {
-        action.setValue(key, value, props);
-    },
-    
-    onFrame: function (bucket, action, values, props, data) {
-        fireCallback('onFrame', bucket, action, values, props, data);
-    },
-    
-    onChange: function (bucket, action, values, props, data) {
-        fireCallback('onChange', bucket, action, values, props, data);
-    },
-    
-    onEnd: function (bucket, action, values, props, data) {
-        fireCallback('onEnd', bucket, action, values, props, data);
-    }
-    
-};
+module.exports = valuesRoute;
