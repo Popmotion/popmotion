02d721f4eb3da23d47dc4e678559478f14fceb12
MattPerry8 <matt.perry@photobox.com>
2015-03-05 11:21:20 +0000

    reverting values

--- redshift.global.js
+++ redshift.global.js
@@ -3009,40 +3009,35 @@ module.exports = templates;
 */
 "use strict";
 
-var generateCallbackFire = function (name) {
-        return function () {
-            fireCallback.apply(this, [].slice.call(arguments).unshift(name));
-        }
-    },
-    
-    fireCallback = function (name, bucket, action, values, props, data) {
+var fireCallback = function (name, bucket, action, values, props, data) {
         if (props[name]) {
             props[name].call(props.scope, bucket, data);
         }
-    },
-    
-    callbacks = ['onChange', 'onFrame', 'onEnd'],
-    
-    valuesRoute = {
-        makeDefault: true,
-        
-        name: 'values',
-        
-        preprocess: function (key, value, action, props) {
-            action.setValue(key, value, props);
-        }
-    },
-    
-    i = 0,
-    
-    callbackName = '';
-    
-for (; i < callbacks.length; i++) {
-    callbackName = callbacks[i];
-    valuesRoute[callbackName] = generateCallbackFire(callbackName);
-}
+    };
 
-module.exports = valuesRoute;
+module.exports = {
+    
+    makeDefault: true,
+    
+    name: 'values',
+    
+    preprocess: function (key, value, action, props) {
+        action.setValue(key, value, props);
+    },
+    
+    onFrame: function (bucket, action, values, props, data) {
+        fireCallback('onFrame', bucket, action, values, props, data);
+    },
+    
+    onChange: function (bucket, action, values, props, data) {
+        fireCallback('onChange', bucket, action, values, props, data);
+    },
+    
+    onEnd: function (bucket, action, values, props, data) {
+        fireCallback('onEnd', bucket, action, values, props, data);
+    }
+    
+};
 },{}],30:[function(require,module,exports){
 (function (global){
 /*
