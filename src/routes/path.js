5f9ab47cf34c43e176e602ccb8b24b31aac62239
MattPerry8 <matt.perry@photobox.com>
2015-03-17 17:27:24 +0000

    path

--- src/routes/path.js
+++ src/routes/path.js
@@ -6,8 +6,21 @@ module.exports = {
     
     onChange: function (output, action, values, props) {
         action.style({
+            'stroke': output.color
+            'stroke-opacity': output.opacity
+            'stroke-dashwidth': output.width
             'stroke-dashoffset': output.start,
-            'stroke-dasharray': output.length + output.spacing // spacing default 100%
+            'stroke-dasharray': output.length + ' ' + output.spacing // spacing default 100%
         });
     }
 };
+
+var myPath = redshift.newAction({
+    dom: yourSVG
+});
+
+myPath.play({
+    svgPath: {
+        width: 100
+    }
+}, 500);
