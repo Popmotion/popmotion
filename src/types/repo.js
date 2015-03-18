d4d4a701c6990cad8865e4e1fad8199f1923e52c
MattPerry8 <matt.perry@photobox.com>
2015-03-18 12:26:17 +0000

    repo rewrite

--- src/types/repo.js
+++ src/types/repo.js
@@ -2,67 +2,67 @@
 
 var utils = require('../utils/utils.js'),
 
-    createRepo = function () {
-        var store = {},
+    /*
+        Get data with specified key
         
-            /*
-                Determine whether call is getter or setter
-            */
-            repository = function () {
-                // If this is a getter, return value
-                if ((!arguments.length || utils.isString(arguments[0]))) {
-                    return this.get.apply(this, arguments);
+        @param [string]: Name of property to access
+        @returns [var]: Data found
+    */
+    get = function (key) {
+        return this[key] ? this[key] : this;
+    },
+            
+    /*
+        Set data either has object or key/value pair
+        
+        Syntax
+            .set(data)
+                @param [object]: Data to store
                 
-                // Or this is a setter, return this
-                } else {
-                    return this.set.apply(this, arguments);
-                }
-            };
+            .set(key, value)
+                @param [string]: Name of data
+                @param [val]: Data to store
+    */
+    set = function (data, prop) {
+        var multiArg = (arguments.length > 1),
+            toSet = multiArg ? {} : data,
+            key = '';
         
-    
-        /*
-            Get data with specified key
-            
-            @param [string]: Name of property to access
-            @returns [var]: Data found
-        */
-        repository.get = function (key) {
-            return store[key] ? store[key] : store;
-        };
-    
-    
-        /*
-            Set data either has object or key/value pair
-            
-            Syntax
-                .set(data)
-                    @param [object]: Data to store
-                    
-                .set(key, value)
-                    @param [string]: Name of data
-                    @param [val]: Data to store
-        */
-        repository.set = function (data, prop) {
-            var multiArg = (arguments.length > 1),
-                toSet = multiArg ? {} : data,
-                key = '';
-            
-            // If this is a key/value setter, add to toSet
-            if (multiArg) {
-                toSet[data] = prop;
-            }
-            
-            // Loop over toSet and assign to our data store
-            for (key in toSet) {
-                if (toSet.hasOwnProperty(key)) {
-                    store[key] = toSet[key];
-                }
-            }
-            
-            return this;
-        };
+        // If this is a key/value setter, add to toSet
+        if (multiArg) {
+            toSet[data] = prop;
+        }
         
-        return repository;
+        // Loop over toSet and assign to our data store
+        for (key in toSet) {
+            if (toSet.hasOwnProperty(key)) {
+                this[key] = toSet[key];
+            }
+        }
+    },
+
+    /*
+        Repo class
+    */
+    Repo = function (context) {
+        var store = {};
+
+        /*
+            Determine whether call is getter or setter
+        */
+        return function () {
+            var argsLength = arguments.length;
+
+            // If this is a getter, return value
+            if ((!argsLength || (argsLength === 1 && utils.isString(arguments[0])))) {
+                return get.apply(store, arguments);
+
+            // Or this is a setter, return this
+            } else {
+                set.apply(store, arguments);
+                return context;
+            }
+        };
     };
 
-module.exports = createRepo;
+module.exports = Repo;
