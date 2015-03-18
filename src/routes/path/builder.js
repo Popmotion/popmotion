2515cba3f40166697e7d7efe2a38868fd0645b83
MattPerry8 <matt.perry@photobox.com>
2015-03-18 16:25:39 +0000

    modularising path route

---
+++ src/routes/path/builder.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var lookup = require('./lookup.js'),
+
+    /*
+        Convert percentage to pixels
+        
+        @param [number]: Percentage of total length
+        @param [number]: Total length
+    */
+    percentToPixels = function (percentage, length) {
+        return (parseFloat(percentage) / 100) * length + 'px';
+    };
+
+/*
+    Create styles
+    
+    @param [object]: SVG Path properties
+    @param [object]: Length of path
+    @returns [object]: Key/value pairs of valid CSS properties
+*/
+module.exports = function (props, pathLength) {
+    var hasArray = false,
+        svgProperty = '',
+        arrayStyles = {
+            length: 0,
+            spacing: pathLength
+        },
+        pathStyles = {};
+
+    // Loop over each property and create related css property
+    for (var key in props) {
+        if (props.hasOwnProperty(key)) {
+            svgProperty = lookup[key];
+            
+            switch (key) {
+                case 'length':
+                case 'spacing':
+                    hasArray = true;
+                    arrayStyles[key] = percentToPixels(props[key], pathLength);
+                    break;
+                case 'offset':
+                    pathStyles[svgProperty] = percentToPixels(props[key], pathLength);
+                    break;
+                default:
+                   pathStyles[svgProperty] = props[key]; 
+            }
+        }
+    }
+    
+    if (hasArray) {
+        pathStyles[DASH_ARRAY] = arrayStyles.length + ' ' + arrayStyles.spacing;
+    }
+    
+    return pathStyles;
+};
---
+++ src/routes/path/lookup.js
@@ -0,0 +1,11 @@
+var STROKE = 'stroke',
+    DASH = STROKE + '-dash',
+    DASH_ARRAY = DASH + 'array'
+
+module.exports = {
+    opacity: STROKE + '-opacity',
+    width: DASH + 'width',
+    offset: DASH + 'offset',
+    length: DASH_ARRAY,
+    spacing: DASH_ARRAY
+};
