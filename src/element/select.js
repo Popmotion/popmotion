bbdb55d2dcad713b254df8d2adf24cc27deda895
Matt Perry <Matt@Winterfell.local>
2015-06-22 22:39:46 +0100

    changing dom to select

--- src/element/select.js
+++ src/element/select.js
@@ -1,7 +1,30 @@
 "use strict";
 
-var utils = require('../inc/utils.js');
+var ElementSystem = require('./ElementSystem');
 
-module.exports = function (items) {
-    
+/*
+    Create an ElementSystem based on a selection of DOM nodes
+
+    @param [string || NodeList || jQuery object]:
+        If string, treated as selector.
+        If not, treated as preexisting NodeList || jQuery object.
+*/
+module.exports = function (selector) {
+    var nodes = (typeof selector === 'string') ? document.querySelectorAll(selector) : selector;
+        elements = [];
+
+    // If jQuery selection, get array of Elements
+    if (nodes.get) {
+        elements = nodes.get();
+
+    // Or convert NodeList to array
+    } else if (nodes.length) {
+        elements = [].slice.call(nodes);
+
+    // Or if it's just an Element, put into array
+    } else {
+        elements.push(nodes);
+    }
+
+    return new ElementSystem(elements);
 };
