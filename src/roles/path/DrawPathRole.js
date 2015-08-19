5c6d63f3b92b20d12ea06eae322f08179d81902b
Matt Perry <MattPerry@Matts-MacBook-Pro.local>
2015-08-19 13:47:11 +0100

    moving back to rules and adding movepathrole

---
+++ src/roles/path/DrawPathRole.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var each = require('../../inc/utils').each;
+
+/*
+    Convert percentage to pixels
+    
+    @param [number]: Percentage of total length
+    @param [number]: Total length
+*/
+var percentToPixels = function (percentage, length) {
+    return (parseFloat(percentage) / 100) * length + 'px';
+}
+
+/*
+    Create styles
+    
+    @param [object]: SVG Path properties
+    @param [object]: Length of path
+    @returns [object]: Key/value pairs of valid CSS properties
+*/
+var createStyles = function (props, length) {
+    var hasDashArray = false,
+        dashArrayStyles = {
+            length: 0,
+            spacing: length + 'px'
+        },
+        styles = {};
+
+    each(props, function (key, value) {
+        key = SVGDrawPath.map[key] || key;
+
+        switch (key) {
+            case 'length',
+            case 'spacing':
+                hasDashArray = true;
+                dashArrayStyles[key] = percentToPixels(value, length);
+                break;
+            case 'offset':
+                styles[key] = percentToPixels(-value, length);
+                break;
+            default:
+                styles[key] = value;
+        }
+    });
+
+    if (hasDashArray) {
+        styles[SVGDrawPath.map.length] = dashArrayStyles.length + ' ' + dashArrayStyles.spacing;
+    }
+
+    return styles;
+};
+
+/*
+    Draw Path role
+*/
+var SVGDrawPath = {
+    map: require('./map'),
+
+    typeMap: {
+        stroke: 'color'
+    },
+
+    init: function () {
+        this.pathLength = this.element.getTotalLength();
+    },
+
+    /*
+        Update `path` styles and if `element` is present, set
+        x, y and rotation
+    */
+    update: function (state) {
+        CSSRole.update.call(actor, createStyles(state, this.pathLength));
+    }
+};
+
+module.exports = SVGDrawPath;
