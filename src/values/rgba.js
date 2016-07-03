fbddaf0efa09cf599a3fc43f117f777fc98921f4
Matt Perry <mattgperry@gmail.com>
2016-07-03 22:12:55 +0100

    latest

--- src/values/rgba.js
+++ src/values/rgba.js
@@ -1,20 +1,26 @@
 import isFirstChar from './utils/is-first-char';
+import splitColorValues from './utils/split-color-values';
 import alpha from './alpha';
 import rgbColor from './rgb-color';
 
+const RED = 'Red';
+const GREEN = 'Green';
+const BLUE = 'Blue';
+const ALPHA = 'Alpha';
+
+const template = (colors) => `rgba(${colors[RED]}, ${colors[GREEN]}, ${colors[BLUE]}, ${colors[ALPHA]})`;
+
 export default {
   childTypes: {
-    Red: rgbColor,
-    Green: rgbColor,
-    Blue: rgbColor,
-    Alpha: alpha
+    [RED]: rgbColor,
+    [GREEN]: rgbColor,
+    [BLUE]: rgbColor,
+    [ALPHA]: alpha
   },
 
   test: isFirstChar('rgb'),
 
-  parse: () => {},
+  parse: splitColorValues([RED, GREEN, BLUE, ALPHA]),
 
-  template: ({ Red, Green, Blue, Alpha }) => `rgba(${Red}, ${Green}, ${Blue}, ${Alpha})`,
-
-  output: () => combineObject()
+  output: template
 }
---
+++ src/values/utils/split-color-values.js
@@ -0,0 +1,16 @@
+import { splitCommaDelimited, getValueFromFunctionString } from '../../inc/utils';
+
+export default function splitColorValues(terms) {
+  const numTerms = terms.length;
+
+  return function (v) {
+    const values = {};
+    const valuesArray = splitCommaDelimited(getValueFromFunctionString(v));
+
+    for (let i = 0; i < numTerms; i++) {
+      values[terms[i]] = (valuesArray[i] !== undefined) ? valuesArray[i] : 1;
+    }
+
+    return values;
+  }
+}
