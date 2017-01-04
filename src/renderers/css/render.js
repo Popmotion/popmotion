05cf5078e75670145ffd9f68e14dfe2aa62197b5
Matt Perry <mattgperry@gmail.com>
2017-01-04 16:15:40 +0000

    reverting to diff method

--- src/renderers/css/render.js
+++ src/renderers/css/render.js
@@ -16,7 +16,30 @@ export default function buildStylePropertyString(element, state, changedValues, 
   let hasTransform = false;
   let transformHasZ = false;
 
-  for (let key in state) {
+  // First check if there are any changed transform values
+  // and if true add all transform values
+  const numChangedValues = changedValues.length;
+  for (let i = 0; i < numChangedValues; i++) {
+    const key = changedValues[i];
+
+    // If this is a transform property, add all other transform props
+    // to changedValues and then break
+    if (transformProps[key]) {
+      hasTransform = true;
+
+      for (let key in state) {
+        if (transformProps[key] && changedValues.indexOf(key) === -1) {
+          changedValues.push(key);
+        }
+      }
+
+      break;
+    }
+  }
+
+  const totalNumChangedValues = changedValues.length;
+  for (let i = 0; i < totalNumChangedValues; i++) {
+    let key = changedValues[i];
     let value = state[key];
 
     if (translateMap[key]) {
@@ -29,7 +52,6 @@ export default function buildStylePropertyString(element, state, changedValues, 
     }
 
     if (transformProps[key]) {
-      hasTransform = true;
       transformString += key + '(' + value + ') ';
       transformHasZ = (key === translateMap.z) ? true : transformHasZ;
 
@@ -37,50 +59,6 @@ export default function buildStylePropertyString(element, state, changedValues, 
       propertyString += ';' + prefixer(key, true) + ':' + value;
     }
   }
-
-  // // First check if there are any changed transform values
-  // // and if true add all transform values
-  // const numChangedValues = changedValues.length;
-  // for (let i = 0; i < numChangedValues; i++) {
-  //   const key = changedValues[i];
-
-  //   // If this is a transform property, add all other transform props
-  //   // to changedValues and then break
-  //   if (transformProps[key]) {
-  //     hasTransform = true;
-
-  //     for (let key in state) {
-  //       if (transformProps[key] && changedValues.indexOf(key) === -1) {
-  //         changedValues.push(key);
-  //       }
-  //     }
-
-  //     break;
-  //   }
-  // }
-
-  // const totalNumChangedValues = changedValues.length;
-  // for (let i = 0; i < totalNumChangedValues; i++) {
-  //   let key = changedValues[i];
-  //   let value = state[key];
-
-  //   if (translateMap[key]) {
-  //     key = translateMap[key];
-  //   }
-
-  //   // If this is a number or object and we have filter, apply filter
-  //   if (valueTypes[key] && (isNum(value) || isObj(value)) && valueTypes[key].transform) {
-  //     value = valueTypes[key].transform(value);
-  //   }
-
-  //   if (transformProps[key]) {
-  //     transformString += key + '(' + value + ') ';
-  //     transformHasZ = (key === translateMap.z) ? true : transformHasZ;
-
-  //   } else {
-  //     propertyString += ';' + prefixer(key, true) + ':' + value;
-  //   }
-  // }
 
   // If we have transform props, build a transform string
   if (hasTransform) {
