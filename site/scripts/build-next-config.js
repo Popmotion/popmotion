8f62f9a85fb77672dd532b03d88bafff26249072
Matt Perry <mattgperry@gmail.com>
2017-06-19 10:33:13 +0100

    adding forward slash to output dir

--- site/scripts/build-next-config.js
+++ site/scripts/build-next-config.js
@@ -24,7 +24,7 @@ const generateRouteDefinitions = (data) => {
     pageIds.forEach((pageId) => {
       routes += `
         '${rootId}/${pageId}': {
-          page: '${rootId}/${pageId}'
+          page: '/${rootId}/${pageId}'
         },
       `;
     });
