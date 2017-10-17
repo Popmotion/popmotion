8304782d945f01184c14051f3e63c83f7cdc42e8
Matt Perry <mattgperry@gmail.com>
2017-10-17 22:00:27 +0100

    composite testing

--- src/compositors/_tests/compositors.test.ts
+++ src/compositors/_tests/compositors.test.ts
@@ -1,9 +1,23 @@
 import action from '../../action';
+import everyFrame from '../../animations/every-frame';
 import chain from '../chain';
+import composite from '../composite';
 
 const fireOne = action(({ complete, update }) => {
   update(1);
   complete();
+});
+
+const createCounter = (start) => action(({ complete, update }) => {
+  let counter = start;
+  everyFrame().start(() => {
+    counter++;
+    if (counter > 3) {
+      complete();
+    } else {
+      update(counter);
+    }
+  });
 });
 
 describe('chain', () => {
@@ -21,4 +35,23 @@ describe('chain', () => {
 
     expect(i).toEqual(2);
   });
+});
+
+describe('composite', () => {
+  it('should create an output of named values from multiple sources', () => {
+    let output = [];
+    
+    return new Promise((resolve) => {
+      composite({ x: createCounter(0), y: createCounter(1) }).start({
+        complete: resolve,
+        update: ({ x, y }) => output.push({ x, y })
+      });
+    }).then(() => {
+      expect(output).toEqual([
+        { x: 1, y: 2 },
+        { x: 2, y: 3 },
+        { x: 3, y: 3 }
+      ]);
+    });
+  })
 });
--- src/compositors/composite.ts
+++ src/compositors/composite.ts
@@ -1,6 +1,6 @@
 import { onFrameUpdate } from 'framesync';
-import action from 'actions/action';
-import { Observable } from 'actions/action/types';
+import action from '../action';
+import { Observable } from '../action/types';
 
 type ObservableMap = {
   [key: string]: Observable
@@ -11,25 +11,24 @@ type ValueMap = {
 };
 
 const composite = (actionMap: ObservableMap) => {
+  const actionKeys = Object.keys(actionMap);
+  const numActions = actionKeys.length;
+
   return action((observer) => {
     const output: ValueMap = {};
     const updateOutput = () => observer.update(output);
-    const keys = Object.keys(actionMap);
-    const numActions = keys.length;
     let numCompletedActions = 0;
 
-    const subs = Object.keys(actionMap).map((key) => {
-      return actionMap[key].start({
-        update: (v: any) => {
-          output[key] = v;
-          onFrameUpdate(updateOutput);
-        },
-        complete: () => {
-          numCompletedActions++;
-          if (numCompletedActions === numActions) observer.complete();
-        }
-      });
-    });
+    const subs = actionKeys.map((key) => actionMap[key].start({
+      complete: () => {
+        numCompletedActions++;
+        if (numCompletedActions === numActions) observer.complete();
+      },
+      update: (v: any) => {
+        output[key] = v;
+        onFrameUpdate(updateOutput);
+      }
+    }));
 
     return {
       stop: () => subs.forEach((sub) => sub.stop())
