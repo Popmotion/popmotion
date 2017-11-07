b4d61bcf8802330e37251ae2fbfaf0be834fd065
Matt Perry <mattgperry@gmail.com>
2017-11-07 18:21:39 +0000

    adding compositor tests

--- packages/popmotion/src/compositors/_tests/compositors.test.ts
+++ packages/popmotion/src/compositors/_tests/compositors.test.ts
@@ -3,6 +3,7 @@ import onFrame from '../../animations/on-frame';
 import chain from '../chain';
 import composite from '../composite';
 import crossFade from '../crossfade';
+import merge from '../merge';
 import parallel from '../parallel';
 
 const fireOne = action(({ complete, update }) => {
@@ -12,12 +13,13 @@ const fireOne = action(({ complete, update }) => {
 
 const createCounter = (start) => action(({ complete, update }) => {
   let counter = start;
+
   onFrame().start(() => {
-    counter++;
     if (counter > start + 3) {
       complete();
     } else {
       update(counter);
+      counter++;
     }
   });
 });
@@ -39,141 +41,80 @@ describe('chain', () => {
   });
 });
 
-// describe('composite', () => {
-//   it('should create an output of named values from multiple sources', () => {
-//     let output = [];
-    
-//     return new Promise((resolve) => {
-//       composite({ x: createCounter(0), y: createCounter(1) }).start({
-//         complete: resolve,
-//         update: ({ x, y }) => output.push({ x, y })
-//       });
-//     }).then(() => {
-//       expect(output).toEqual([
-//         { x: 1, y: 2 },
-//         { x: 2, y: 2 },
-//         { x: 2, y: 3 },
-//         { x: 3, y: 3 },
-//         { x: 3, y: 4 }
-//       ]);
-//     });
-//   })
-// });
+describe('crossfade', () => {
+  it('should blend the left action by default', () => {
+    return new Promise((resolve, reject) => {
+      const i = 0;
+      crossFade(createCounter(0), createCounter(1)).start({
+        complete: () => i === 2 ? resolve() : reject(i),
+        update: (v) => i = v
+      });
+    });
+  });
+  it('should correctly blend both actions', () => {
+    return new Promise((resolve, reject) => {
+      const i = 0;
+      crossFade(createCounter(0), createCounter(1)).start({
+        complete: () => i === 4.5 ? resolve() : reject(i),
+        update: (v) => i = v
+      }).setBalance(0.5);
+    });
+  });
+});
 
-// describe('crossfade', () => {
-//   it('should blend the left action by default', () => {
-//     return new Promise((resolve, reject) => {
-//       const i = 0;
-//       crossFade(createCounter(0), createCounter(1)).start({
-//         complete: () => i === 2 ? resolve() : reject(),
-//         update: (v) => i = v
-//       });
-//     });
-//   });
-//   it('should correctly blend both actions', () => {
-//     return new Promise((resolve, reject) => {
-//       const i = 0;
-//       crossFade(createCounter(0), createCounter(1)).start({
-//         complete: () => i === 4.5 ? resolve() : reject(),
-//         update: (v) => i = v
-//       }).setBalance(0.5);
-//     });
-//   });
-// });
+describe('parallel', () => {
+  it('should output two actions in parallel', () => {
+    const output = [];
+    return new Promise((resolve, reject) => {
+      parallel(createCounter(0), createCounter(1)).start({
+        complete: resolve,
+        update: ([ x, y ]) => output.push([ x, y ])
+      });
+    }).then(() => {
+      expect(output).toEqual([
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [3, 4]
+      ]);
+    });
+  });
+});
 
-// describe('parallel', () => {
-//   it('should output two actions in parallel', () => {
-//     let output = [];
-//     return new Promise((resolve, reject) => {
-//       parallel(createCounter(0), createCounter(1)).start({
-//         complete: resolve,
-//         update: ([ x, y ]) => output.push([ x, y ])
-//       });
-//     }).then(() => {
-//       expect(output).toEqual([
-//         [0, 1],
-//         [1, 2],
-//         [2, 3],
-//         [3, 4]
-//       ]);
-//     });
-//   });
-// });
+describe('composite', () => {
+  it('should create an output of named values from multiple sources', () => {
+    const output = [];
 
-// describe('chain', () => {
-//   it('should chain multiple actions', () => {
-//     let i = 0;
-//     chain(fireOne, fireOne, fireOne).start((v) => i = i + v);
-//     expect(i).toEqual(3);
-//   });
+    return new Promise((resolve) => {
+      composite({ x: createCounter(0), y: createCounter(1) }).start({
+        complete: resolve,
+        update: ({ x, y }) => output.push({ x, y })
+      });
+    }).then(() => {
+      expect(output).toEqual([
+        { x: 0, y: 1 },
+        { x: 1, y: 2 },
+        { x: 2, y: 3 },
+        { x: 3, y: 4 }
+      ]);
+    });
+  });
+});
 
-//   it('should stop actions', () => {
-//     let i = 0;
-//     const a = chain(fireOne, fireOne, fireOne, fireOne)
-//       .while((v) => i <= 1)
-//       .start((v) => i = i + v);
+describe('merge', () => {
+  it('should create a single action that outputs values from both sources', () => {
+    const output = [];
 
-//     expect(i).toEqual(2);
-//   });
-// });
-
-// describe('composite', () => {
-//   it('should create an output of named values from multiple sources', () => {
-//     let output = [];
-    
-//     return new Promise((resolve) => {
-//       composite({ x: createCounter(0), y: createCounter(1) }).start({
-//         complete: resolve,
-//         update: ({ x, y }) => output.push({ x, y })
-//       });
-//     }).then(() => {
-//       expect(output).toEqual([
-//         { x: 1, y: 2 },
-//         { x: 2, y: 2 },
-//         { x: 2, y: 3 },
-//         { x: 3, y: 3 },
-//         { x: 3, y: 4 }
-//       ]);
-//     });
-//   })
-// });
-
-// describe('crossfade', () => {
-//   it('should blend the left action by default', () => {
-//     return new Promise((resolve, reject) => {
-//       const i = 0;
-//       crossFade(createCounter(0), createCounter(1)).start({
-//         complete: () => i === 2 ? resolve() : reject(),
-//         update: (v) => i = v
-//       });
-//     });
-//   });
-//   it('should correctly blend both actions', () => {
-//     return new Promise((resolve, reject) => {
-//       const i = 0;
-//       crossFade(createCounter(0), createCounter(1)).start({
-//         complete: () => i === 4.5 ? resolve() : reject(),
-//         update: (v) => i = v
-//       }).setBalance(0.5);
-//     });
-//   });
-// });
-
-// describe('parallel', () => {
-//   it('should output two actions in parallel', () => {
-//     let output = [];
-//     return new Promise((resolve, reject) => {
-//       parallel(createCounter(0), createCounter(1)).start({
-//         complete: resolve,
-//         update: ([ x, y ]) => output.push([ x, y ])
-//       });
-//     }).then(() => {
-//       expect(output).toEqual([
-//         [0, 1],
-//         [1, 2],
-//         [2, 3],
-//         [3, 4]
-//       ]);
-//     });
-//   });
-// });
+    return new Promise((resolve) => {
+      merge(
+        createCounter(0),
+        createCounter(1)
+      ).start({
+        complete: resolve,
+        update: (v) => output.push(v)
+      });
+    }).then(() => {
+      expect(output).toEqual([0, 1, 1, 2, 2, 3, 3, 4]);
+    });
+  });
+});
