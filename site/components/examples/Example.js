09788a85681f67e69270f40816a51b6ffb08f83f
Matt Perry <mattgperry@gmail.com>
2017-11-21 11:30:35 +0000

    latest favicon styles

--- site/components/examples/Example.js
+++ site/components/examples/Example.js
@@ -103,7 +103,6 @@ export default ({ children, template, id, isReactComponent=false }) => {
       <CodeContainer>
         <StyledLiveEditor />
       </CodeContainer>
-      <LiveError />
     </StyledLiveContainer>
   );
 };
