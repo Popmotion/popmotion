533534cdffa2c4ccab1fcf237b3a6dd7a8ea5937
JIANG Di <jiang-di@outlook.com>
2018-01-30 16:29:39 +0800

    the Apply function should return type any

--- packages/popmotion/src/transformers.ts
+++ packages/popmotion/src/transformers.ts
@@ -98,7 +98,7 @@ export const pipe = (...transformers: Function[]) => transformers.reduce(combine
  * Conditionally apply a transformer using the provided function when `check` returns `true`
  */
 export type Check = (v: any) => boolean;
-export type Apply = (v: any) => number;
+export type Apply = (v: any) => any;
 export const conditional = (check: Check, apply: Apply) => (v: any): any => check(v) ? apply(v) : v;
 
 /**
