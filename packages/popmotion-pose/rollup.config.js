4eef9cbf8b9a3609d35a48821664daa7b6810817
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-05-21 14:51:03 +0200

    Tweaked build setups - omit rollup warnings, annotate pure calls & more (#340)

--- packages/popmotion-pose/rollup.config.js
+++ packages/popmotion-pose/rollup.config.js
@@ -3,14 +3,27 @@ import uglify from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
 
 const typescriptConfig = { cacheRoot: 'tmp/.rpt2_cache' };
 const noDeclarationConfig = Object.assign({}, typescriptConfig, {
   tsconfigOverride: { compilerOptions: { declaration: false } }
 });
 
+const makeExternalPredicate = externalArr => {
+  if (externalArr.length === 0) {
+    return () => false;
+  }
+  const pattern = new RegExp(`^(${externalArr.join("|")})($|/)`);
+  return id => pattern.test(id);
+};
+
+const deps = Object.keys(pkg.dependencies || {})
+const peerDeps = Object.keys(pkg.peerDependencies || {})
+
 const config = {
-  input: 'src/index.ts'
+  input: 'src/index.ts',
+  external: makeExternalPredicate(deps.concat(peerDeps))
 };
 
 const umd = Object.assign({}, config, {
@@ -23,10 +36,12 @@ const umd = Object.assign({}, config, {
       'style-value-types': 'valueTypes'
     }
   },
+  external: makeExternalPredicate(peerDeps),
   plugins: [
     typescript(noDeclarationConfig),
+    resolve(),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify('development')
     }),
     commonjs()
   ]
@@ -41,7 +56,7 @@ const umdProd = Object.assign({}, umd, {
     typescript(noDeclarationConfig),
     resolve(),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     uglify(),
     commonjs()
@@ -50,7 +65,7 @@ const umdProd = Object.assign({}, umd, {
 
 const es = Object.assign({}, config, {
   output: {
-    file: 'dist/popmotion-pose.es.js',
+    file: pkg.module,
     format: 'es',
     exports: 'named'
   },
@@ -59,7 +74,7 @@ const es = Object.assign({}, config, {
 
 const cjs = Object.assign({}, config, {
   output: {
-    file: 'lib/index.js',
+    file: pkg.main,
     format: 'cjs',
     exports: 'named'
   },
