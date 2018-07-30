9e5af38efb9b499a71beb1d26445e2cc888e8346
Matt Perry <mattgperry@gmail.com>
2018-07-30 14:11:14 +0100

    removing unused var

--- packages/popmotion/src/chainable/index.ts
+++ packages/popmotion/src/chainable/index.ts
@@ -14,27 +14,25 @@ export default abstract class Chainable<T> {
   applyMiddleware(middleware: Middleware): T {
     return this.create({
       ...this.props,
-      middleware: this.props.middleware ? [middleware, ...this.props.middleware] : [middleware]
+      middleware: this.props.middleware
+        ? [middleware, ...this.props.middleware]
+        : [middleware]
     });
   }
 
   pipe(...funcs: Update[]): T {
     const pipedUpdate = funcs.length === 1 ? funcs[0] : pipe(...funcs);
 
-    return this.applyMiddleware(
-      (update) => (v) => update(pipedUpdate(v))
-    );
+    return this.applyMiddleware(update => v => update(pipedUpdate(v)));
   }
 
   while(predicate: Predicate): T {
-    return this.applyMiddleware(
-      (update, complete) => (v) => predicate(v) ? update(v) : complete()
+    return this.applyMiddleware((update, complete) => v =>
+      predicate(v) ? update(v) : complete()
     );
   }
 
   filter(predicate: Predicate): T {
-    return this.applyMiddleware(
-      (update, complete) => (v) => predicate(v) && update(v)
-    );
+    return this.applyMiddleware(update => v => predicate(v) && update(v));
   }
 }
