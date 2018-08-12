b7eb5e943da3fc659f0af5d6a39022c3cffd7009
Hendrik Putzek <hendrik@putzek.com>
2018-08-12 14:35:56 +0200

    [Documentation] fix link targets for links to stylefire docs (#417)
    Links to stylers in the stylefire documentation point to the right URL now.

--- packages/site/pages/pure/api/svg.js
+++ packages/site/pages/pure/api/svg.js
@@ -35,7 +35,7 @@ const convertMarkdown = marksy({
 });
 
 const content = convertMarkdown(`
-[Moved to the Stylefire docs](/stylefire/api/styler/svg).`);
+[Moved to the Stylefire docs](/stylefire/api/svg).`);
 
 const Page = ({ section }) => (
   <ContentTemplate
