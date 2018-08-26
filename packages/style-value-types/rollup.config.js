da6ff7c797801f3d31e980580a142cea90617451
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-08-26 12:36:38 +0200

    Tweak style-value-types build

--- packages/style-value-types/rollup.config.js
+++ packages/style-value-types/rollup.config.js
@@ -1,45 +1,67 @@
 import typescript from 'rollup-plugin-typescript2';
 import uglify from 'rollup-plugin-uglify';
+import pkg from './package.json';
 
-const typescriptConfig = { cacheRoot: 'tmp/.rpt2_cache' };
+const ensureArray = maybeArr => Array.isArray(maybeArr) ? maybeArr : [maybeArr];
 
-const config = {
-  input: 'src/index.ts'
+const makeExternalPredicate = externalArr => {
+  if (externalArr.length === 0) {
+    return () => false;
+  }
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
+  return id => pattern.test(id);
 };
 
-const umd = Object.assign({}, config, {
-  output: {
-    file: 'dist/style-value-types.js',
-    format: 'umd',
+const deps = Object.keys(pkg.dependencies || {});
+const peerDeps = Object.keys(pkg.peerDependencies || {});
+
+const createConfig = ({
+  output,
+  external = 'all',
+  min = false,
+}) => ({
+  input: 'src/index.ts',
+  output: ensureArray(output).map(format => Object.assign({
     name: 'valueTypes',
-    exports: 'named'
-  },
-  plugins: [typescript(typescriptConfig)]
+    exports: 'named',
+  }, format)),
+  external: makeExternalPredicate(external === 'peers' ? peerDeps : deps.concat(peerDeps)),
+  plugins: [
+    typescript({
+      cacheRoot: 'tmp/.rpt2_cache',
+      include: /\.tsx?$/,
+      tsconfigOverride: { compilerOptions: { resolve: false } },
+    }),
+    min && uglify(),
+  ].filter(Boolean)
 });
 
-const umdProd = Object.assign({}, umd, {
-  output: Object.assign({}, umd.output, {
-    file: 'dist/style-value-types.min.js'
+export default [
+  createConfig({
+    output: [{
+      file: pkg.module,
+      format: 'es',
+    }, {
+      file: pkg.main,
+      format: 'cjs',
+    }],
   }),
-  plugins: [typescript(typescriptConfig), uglify()]
-});
+  createConfig({
+    output: {
+      file: pkg.unpkg,
+      format: 'umd',
+    },
+    external: 'peers',
+    env: 'production',
+    min: true,
+  }),
+  createConfig({
+    output: {
+      file: pkg.unpkg.replace(/\.min\.js$/, '.js'),
+      format: 'umd',
+    },
+    external: 'peers',
+    env: 'development',
+  })
+];
 
-const es = Object.assign({}, config, {
-  output: {
-    file: 'dist/style-value-types.es.js',
-    format: 'es',
-    exports: 'named'
-  },
-  plugins: [typescript(typescriptConfig)]
-});
-
-const cjs = Object.assign({}, config, {
-  output: {
-    file: 'lib/index.js',
-    format: 'cjs',
-    exports: 'named'
-  },
-  plugins: [typescript(typescriptConfig)]
-});
-
-export default [umd, umdProd, es, cjs];
