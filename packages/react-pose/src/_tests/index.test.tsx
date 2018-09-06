d7c5d67211219237c50966fc1bdb0cc1b719e33a
Matt Perry <mattgperry@gmail.com>
2018-09-06 12:03:24 +0100

    adding react tests

--- packages/react-pose/src/_tests/index.test.tsx
+++ packages/react-pose/src/_tests/index.test.tsx
@@ -1,14 +1,228 @@
 import React from 'react';
 import posed from '../posed';
+import { PoseGroup } from '../components/PoseGroup';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('posed', () => {
-  const Test = posed.div();
+const Parent = posed.div({
+  init: { x: 10 },
+  foo: { x: 20 },
+  bar: { x: 30 },
+  fromProps: { x: ({ i }) => i },
+  preEnter: { x: 40 },
+  enter: { x: 50 },
+  exit: { x: 60 }
+});
+const Child = posed.div({
+  init: { y: 15 },
+  foo: { y: 25 },
+  bar: { y: 35 },
+  fromProps: { y: ({ i }) => i },
+  preEnter: { y: 45 },
+  enter: { y: 55 },
+  exit: { y: 65 }
+});
 
-  const wrapper = mount(<Test pose="init" />);
-  expect(wrapper.props().pose).toBe('init');
+test('posed: initial state', () => {
+  let x = 0;
+  let y = 0;
+
+  const namedInit = mount(
+    <Parent pose="foo" onValueChange={{ x: v => (x = v) }}>
+      <Child onValueChange={{ y: v => (y = v) }} />
+    </Parent>
+  );
+
+  expect(namedInit.props().pose).toBe('foo');
+  expect(x).toBe(20);
+  expect(y).toBe(25);
+
+  mount(
+    <Parent onValueChange={{ x: v => (x = v) }}>
+      <Child onValueChange={{ y: v => (y = v) }} />
+    </Parent>
+  );
+
+  expect(x).toBe(10);
+  expect(y).toBe(15);
+});
+
+test('posed: mount animation with `initialPose`', () => {
+  let x = 0;
+  let y = 0;
+
+  return new Promise(resolve => {
+    mount(
+      <Parent
+        initialPose="bar"
+        pose="foo"
+        onPoseComplete={() => {
+          expect(x).toBe(20);
+          expect(y).toBe(25);
+          resolve();
+        }}
+        onValueChange={{ x: v => (x = v) }}
+      >
+        <Child onValueChange={{ y: v => (y = v) }} />
+      </Parent>
+    );
+
+    expect(x).toBe(30);
+    expect(y).toBe(35);
+  });
+});
+
+test('posed: passes through props', () => {
+  let x = 0;
+  let y = 0;
+
+  mount(
+    <Parent pose="fromProps" i={1} onValueChange={{ x: v => (x = v) }}>
+      <Child i={2} onValueChange={{ y: v => (y = v) }} />
+    </Parent>
+  );
+
+  expect(x).toBe(1);
+  expect(y).toBe(2);
+});
+
+test('PoseGroup: Initial visibility (visible)', () => {
+  let x = 0;
+  let y = 0;
+
+  const Group = ({ isVisible = true }) => (
+    <PoseGroup>
+      {isVisible && (
+        <Parent key="a" onValueChange={{ x: v => (x = v) }}>
+          <Child onValueChange={{ y: v => (y = v) }} />
+        </Parent>
+      )}
+    </PoseGroup>
+  );
+
+  mount(<Group />);
+
+  expect(x).toBe(50);
+  expect(y).toBe(55);
+});
+
+test('PoseGroup: Initial visibility (hidden)', () => {
+  let x = 0;
+  let y = 0;
+
+  const Group = ({ isVisible = false }) => (
+    <PoseGroup>
+      {isVisible && (
+        <Parent key="a" onValueChange={{ x: v => (x = v) }}>
+          <Child onValueChange={{ y: v => (y = v) }} />
+        </Parent>
+      )}
+    </PoseGroup>
+  );
+
+  mount(<Group />);
+
+  expect(x).toBe(0);
+  expect(y).toBe(0);
+});
+
+test('PoseGroup: Animate on mount', () => {
+  let x = 0;
+  let y = 0;
+
+  return new Promise(resolve => {
+    const Group = ({ isVisible = true }) => (
+      <PoseGroup animateOnMount>
+        {isVisible && (
+          <Parent
+            key="a"
+            onPoseComplete={() => {
+              expect(x).toBe(50);
+              expect(y).toBe(55);
+              resolve();
+            }}
+            onValueChange={{ x: v => (x = v) }}
+          >
+            <Child onValueChange={{ y: v => (y = v) }} />
+          </Parent>
+        )}
+      </PoseGroup>
+    );
+
+    mount(<Group />);
+
+    expect(x).toBe(60);
+    expect(y).toBe(65);
+  });
+});
+
+test('PoseGroup: Animate conditionally', () => {
+  let x = 0;
+  let y = 0;
+
+  return new Promise(resolve => {
+    const Group = ({ isVisible = false }) => (
+      <PoseGroup animateOnMount>
+        {isVisible && (
+          <Parent
+            key="a"
+            onPoseComplete={() => {
+              expect(x).toBe(50);
+              expect(y).toBe(55);
+              resolve();
+            }}
+            onValueChange={{ x: v => (x = v) }}
+          >
+            <Child onValueChange={{ y: v => (y = v) }} />
+          </Parent>
+        )}
+      </PoseGroup>
+    );
+
+    const wrapper = mount(<Group />);
+
+    expect(x).toBe(0);
+    expect(y).toBe(0);
+
+    wrapper.setProps({ isVisible: true });
+    expect(x).toBe(60);
+    expect(y).toBe(65);
+  });
+});
+
+test('PoseGroup: special pre-enter pose', () => {
+  let x = 0;
+  let y = 0;
+
+  return new Promise(resolve => {
+    const Group = ({ isVisible = false }) => (
+      <PoseGroup animateOnMount preEnterPose="foo">
+        {isVisible && (
+          <Parent
+            key="a"
+            onPoseComplete={() => {
+              expect(x).toBe(50);
+              expect(y).toBe(55);
+              resolve();
+            }}
+            onValueChange={{ x: v => (x = v) }}
+          >
+            <Child onValueChange={{ y: v => (y = v) }} />
+          </Parent>
+        )}
+      </PoseGroup>
+    );
+
+    const wrapper = mount(<Group />);
+
+    expect(x).toBe(0);
+    expect(y).toBe(0);
+
+    wrapper.setProps({ isVisible: true });
+    expect(x).toBe(20);
+    expect(y).toBe(25);
+  });
 });
