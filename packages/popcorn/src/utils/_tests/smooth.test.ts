a468734c0e8e89b8d759b6b07a122e881112075c
Matt Perry <mattgperry@gmail.com>
2018-10-03 11:24:05 +0100

    disabling tests

--- packages/popcorn/src/utils/_tests/smooth.test.ts
+++ packages/popcorn/src/utils/_tests/smooth.test.ts
@@ -1,18 +1,17 @@
-// import smooth from '../smooth';
-// import sync from 'framesync';
+import smooth from '../smooth';
+import sync from 'framesync';
 
-// TODO: Investigate why this test hangs CircleCI
-// test('smooth', () => {
-//   const smoother = smooth();
+test('smooth', () => {
+  const smoother = smooth();
 
-//   return new Promise(resolve => {
-//     sync.update(() => {
-//       expect(smoother(100)).toBe(100);
+  return new Promise(resolve => {
+    sync.update(() => {
+      expect(smoother(100)).toBe(100);
 
-//       sync.update(() => {
-//         expect(smoother(200)).toBeGreaterThan(100);
-//         resolve();
-//       });
-//     });
-//   });
-// });
+      sync.update(() => {
+        expect(smoother(200)).toBeGreaterThan(100);
+        resolve();
+      });
+    });
+  });
+});
