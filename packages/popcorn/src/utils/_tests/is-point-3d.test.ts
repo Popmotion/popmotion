bd7b1e4bfe8d890deacc76f3745b8294aeaaf990
Matt Perry <mattgperry@gmail.com>
2018-10-03 11:26:34 +0100

    improving isPoint3D check

--- packages/popcorn/src/utils/_tests/is-point-3d.test.ts
+++ packages/popcorn/src/utils/_tests/is-point-3d.test.ts
@@ -4,4 +4,5 @@ test('isPoint3D', () => {
   expect(isPoint3D(9)).toBe(false);
   expect(isPoint3D({ x: 0, y: 0 })).toBe(false);
   expect(isPoint3D({ x: 0, y: 0, z: 0 })).toBe(true);
+  expect(isPoint3D({ z: 0 })).toBe(false);
 });
