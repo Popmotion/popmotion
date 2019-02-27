7526d216d292c48bc0f1cfb2884e3fc641de0f18
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-02-27 23:47:49 +0100

    Add bounce easings

--- packages/easing/src/index.ts
+++ packages/easing/src/index.ts
@@ -72,6 +72,34 @@ export const backInOut = mirrored(backIn);
 
 export const anticipate = createAnticipateEasing(DEFAULT_OVERSHOOT_STRENGTH);
 
+const BOUNCE_FIRST_THRESHOLD = 4.0 / 11.0;
+const BOUNCE_SECOND_THRESHOLD = 8.0 / 11.0;
+const BOUNCE_THIRD_THRESHOLD = 9.0 / 10.0;
+
+// helper constants
+const ca = 4356.0 / 361.0;
+const cb = 35442.0 / 1805.0;
+const cc = 16061.0 / 1805.0;
+
+export const bounceOut = (p: number) => {
+  const p2 = p * p;
+
+  return p < BOUNCE_FIRST_THRESHOLD
+    ? 7.5625 * p2
+    : p < BOUNCE_SECOND_THRESHOLD
+    ? 9.075 * p2 - 9.9 * p + 3.4
+    : p < BOUNCE_THIRD_THRESHOLD
+    ? ca * p2 - cb * p + cc
+    : 10.8 * p * p - 20.52 * p + 10.72;
+};
+
+export const bounceIn = (p: number) => 1.0 - bounceOut(1.0 - p);
+
+export const bounceInOut = (p: number) =>
+  p < 0.5
+    ? 0.5 * (1.0 - bounceOut(1.0 - p * 2.0))
+    : 0.5 * bounceOut(p * 2.0 - 1.0) + 0.5;
+
 /*
   Bezier function generator
 
