b472e4a65222fffbf11c8473569c93b3cd2bbcf9
InventingWithMonster <mattgperry@gmail.com>
2019-06-26 11:30:59 +0200

    exporting buildSVGAttrs

--- packages/stylefire/src/svg/build.ts
+++ packages/stylefire/src/svg/build.ts
@@ -3,7 +3,7 @@ import { px } from 'style-value-types';
 import { Dimensions, SVGState } from './types';
 import { camelCaseAttributes } from './attr-formatting';
 import { createStyleBuilder } from '../css/build-styles';
-import { State } from '../styler/types';
+import { State, ResolvedState } from '../styler/types';
 
 export type SVGAttrs = {
   [key: string]: any;
@@ -13,8 +13,9 @@ export type SVGAttrs = {
   };
 };
 
-// // Little hack to avoid division by zero
-// const ZERO_NOT_ZERO = 0.0000001;
+const svgAttrsTemplate = (): SVGAttrs => ({
+  style: {}
+});
 
 const progressToPixels = (progress: number, length: number) =>
   px.transform(progress * length);
@@ -44,63 +45,69 @@ function calculateSVGTransformOrigin(
   )}`;
 }
 
-export function createAttrBuilder(
-  dimensions: Dimensions,
-  totalPathLength?: number
-) {
-  const attrs: SVGAttrs = {
-    style: {}
-  };
-
-  const cssBuilder = createStyleBuilder(false, false);
-
-  return ({
+export function buildSVGAttrs(
+  {
     x,
     y,
     z,
     originX = 0.5,
     originY = 0.5,
     pathLength,
-    pathSpacing = totalPathLength,
+    pathSpacing = 1,
     pathOffset = 0,
     ...state
-  }: State & SVGState) => {
-    const style = cssBuilder(state);
+  }: State & SVGState,
+  dimensions: Dimensions,
+  totalPathLength?: number | undefined,
+  cssBuilder: (state: State) => ResolvedState = createStyleBuilder(
+    false,
+    false
+  ),
+  attrs: SVGAttrs = svgAttrsTemplate()
+) {
+  const style = cssBuilder(state);
 
-    for (const key in style) {
-      if (key === 'transform') {
-        attrs.style.transform = style[key] as string;
-      } else {
-        let attrKey = !camelCaseAttributes.has(key) ? camelToDash(key) : key;
-        attrs[attrKey] = style[key];
-      }
+  for (const key in style) {
+    if (key === 'transform') {
+      attrs.style.transform = style[key] as string;
+    } else {
+      let attrKey = !camelCaseAttributes.has(key) ? camelToDash(key) : key;
+      attrs[attrKey] = style[key];
     }
+  }
 
-    // Parse transformOrigin
-    if (originX !== undefined || originY !== undefined) {
-      attrs.style.transformOrigin = calculateSVGTransformOrigin(
-        dimensions,
-        originX as number,
-        originY as number
-      );
-    }
+  // Parse transformOrigin
+  if (originX !== undefined || originY !== undefined) {
+    attrs.style.transformOrigin = calculateSVGTransformOrigin(
+      dimensions,
+      originX as number,
+      originY as number
+    );
+  }
 
-    // Treat x/y not as shortcuts but as actual attributes
-    if (x !== undefined) attrs.x = x;
-    if (y !== undefined) attrs.y = y;
+  // Treat x/y not as shortcuts but as actual attributes
+  if (x !== undefined) attrs.x = x;
+  if (y !== undefined) attrs.y = y;
 
-    // Handle special path length attributes
-    if (totalPathLength !== undefined && pathLength !== undefined) {
-      attrs['stroke-dashoffset'] = progressToPixels(
-        -pathOffset,
-        totalPathLength
-      );
-      attrs['stroke-dasharray'] = `${progressToPixels(
-        pathLength,
-        totalPathLength
-      )} ${progressToPixels(pathSpacing, totalPathLength)}`;
-    }
+  // Handle special path length attributes
+  if (totalPathLength !== undefined && pathLength !== undefined) {
+    attrs['stroke-dashoffset'] = progressToPixels(-pathOffset, totalPathLength);
+    attrs['stroke-dasharray'] = `${progressToPixels(
+      pathLength,
+      totalPathLength
+    )} ${progressToPixels(pathSpacing, totalPathLength)}`;
+  }
 
-    return attrs;
-  };
+  return attrs;
+}
+
+export function createAttrBuilder(
+  dimensions: Dimensions,
+  totalPathLength?: number
+) {
+  const attrs: SVGAttrs = svgAttrsTemplate();
+  const cssBuilder = createStyleBuilder(false, false);
+
+  return (state: State & SVGState) =>
+    buildSVGAttrs(state, dimensions, totalPathLength, cssBuilder, attrs);
 }
