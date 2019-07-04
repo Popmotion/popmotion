7e0e5912bb2aeb288ce3a6b7461bad0feff945e9
InventingWithMonster <mattgperry@gmail.com>
2019-07-04 21:58:51 +0200

    removing array and object actions, stripping value from inertia

--- packages/popmotion/src/action/index.ts
+++ packages/popmotion/src/action/index.ts
@@ -1,7 +1,20 @@
-import Chainable from '../chainable';
 import createObserver from '../observer';
-import { ObserverCandidate, PartialObserver } from '../observer/types';
+import {
+  Middleware,
+  ObserverCandidate,
+  PartialObserver,
+  Update
+} from '../observer/types';
 import { ActionInit, ActionProps, ColdSubscription } from './types';
+import { Props } from '../chainable/types';
+import { pipe } from '@popmotion/popcorn';
+
+export class Action {
+  props: Props;
+
+  constructor(props: Props = {}) {
+    this.props = props;
+  }
 
 export class Action<
   Sub extends ColdSubscription = ColdSubscription
@@ -33,6 +46,21 @@ export class Action<
     if (isComplete) subscription.stop();
 
     return subscription as any;
+  }
+
+  applyMiddleware(middleware: Middleware): Action {
+    return this.create({
+      ...this.props,
+      middleware: this.props.middleware
+        ? [middleware, ...this.props.middleware]
+        : [middleware]
+    } as ActionProps);
+  }
+
+  pipe(...funcs: Update[]): Action {
+    const pipedUpdate = funcs.length === 1 ? funcs[0] : pipe(...funcs);
+
+    return this.applyMiddleware(update => v => update(pipedUpdate(v)));
   }
 }
 
