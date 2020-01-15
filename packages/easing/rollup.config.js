3b5adba1d453ae85557911b11c1272d8aacdf63b
InventingWithMonster <mattgperry@gmail.com>
2020-01-15 16:56:29 +0100

    Moving rollup configs to generator

--- packages/easing/rollup.config.js
+++ packages/easing/rollup.config.js
@@ -1,40 +1,4 @@
-import typescript from '@rollup/plugin-typescript';
+import generateConfig from '../../rollup-generate-config';
 import pkg from './package.json';
 
-const makeExternalPredicate = externalArr => {
-  if (externalArr.length === 0) {
-    return () => false;
-  }
-  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
-  return id => pattern.test(id);
-};
-
-const deps = Object.keys(pkg.dependencies || {});
-const peerDeps = Object.keys(pkg.peerDependencies || {});
-
-const config = {
-  input: 'src/index.ts',
-  external: makeExternalPredicate(deps.concat(peerDeps))
-};
-
-const es = {
-  ...config,
-  input: 'src/index.ts',
-  output: {
-    file: pkg.module,
-    dir: 'dist',
-    format: 'es'
-  },
-  plugins: [typescript()]
-};
-
-const cjs = Object.assign({}, config, {
-  output: {
-    file: pkg.main,
-    format: 'cjs',
-    exports: 'named'
-  },
-  plugins: [typescript()]
-});
-
-export default [es, cjs];
+export default generateConfig(pkg, 'easing');
