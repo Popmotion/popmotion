7247a41244989970d9b95c87dbc351df834b8a2d
InventingWithMonster <mattgperry@gmail.com>
2020-01-20 16:00:54 +0100

    Latest worklet changes

--- packages/popmotion/src/worklet/index.ts
+++ packages/popmotion/src/worklet/index.ts
@@ -1,5 +1,6 @@
 import { tween } from '../animations/tween';
 import { ForT } from '../types';
+import { spring } from '../animations/spring';
 
 interface WorkletAnimationEffect {
   localTime: number;
@@ -8,18 +9,25 @@ interface WorkletAnimationEffect {
 // extends StatelessAnimator when browsers implement the spec
 class StatelessPopmotionAnimator {
   animation: ForT;
+  a: any;
 
-  constructor(animation: ForT) {
+  constructor(animation: ForT, state: any) {
     this.animation = animation;
+    this.state = state;
   }
 
   animate(currentTime: number, effect: WorkletAnimationEffect) {
     // TODO: Why does this snap back once the duration is reached?
     effect.localTime = this.animation(currentTime);
+    this.a = { velocity: 'localTime ' };
+  }
+
+  state() {
+    return this.a;
   }
 }
 
-export interface TweenOptions {}
+interface TweenOptions {}
 
 class Tween extends StatelessPopmotionAnimator {
   constructor(options: TweenOptions) {
@@ -27,5 +35,37 @@ class Tween extends StatelessPopmotionAnimator {
   }
 }
 
+interface SpringOptions {}
+
+class Spring extends StatelessPopmotionAnimator {
+  constructor(options: SpringOptions) {
+    super(spring({}));
+  }
+}
+
 // @ts-ignore
 registerAnimator('tween', Tween);
+
+// @ts-ignore
+registerAnimator('spring', Spring);
+
+interface ParallaxOptions {
+  factor: number;
+}
+
+class Parallax {
+  factor = 0.5;
+
+  constructor(options: ParallaxOptions) {
+    this.factor = options.factor;
+  }
+
+  animate(currentTime: number, effect: WorkletAnimationEffect) {
+    if (isNaN(currentTime)) return;
+
+    effect.localTime = this.factor * currentTime;
+  }
+}
+
+// @ts-ignore
+registerAnimator('parallax', Parallax);
