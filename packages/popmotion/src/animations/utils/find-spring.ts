21803afe3d19909c41c39437aac5b063b07e0f8a
InventingWithMonster <mattgperry@gmail.com>
2020-05-06 09:58:22 +0200

    Latest spring changes

---
+++ packages/popmotion/src/animations/utils/find-spring.ts
@@ -0,0 +1,11 @@
+function findSpringForDuration(
+  duration: number,
+  dampingRatio: number
+): SpringConfig {
+  // try every spring of a certain stiffness -> ratio and search for spring based on duration
+}
+
+findSpringForDuration({
+  duration: 2,
+  dampingRatio: 0.8
+});
