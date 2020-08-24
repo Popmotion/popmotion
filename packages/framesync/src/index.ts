c53ae7e217d567a302b18b908c32020ede04c4c2
InventingWithMonster <mattgperry@gmail.com>
2020-08-24 17:18:46 +0200

    Upgrading dev dependencies

--- packages/framesync/src/index.ts
+++ packages/framesync/src/index.ts
@@ -1,84 +1,81 @@
-import onNextFrame from './on-next-frame';
-import createStep from './create-render-step';
-import { Process, StepId, SyncApi, FrameData } from './types';
+import onNextFrame from "./on-next-frame"
+import createStep from "./create-render-step"
+import { Process, StepId, SyncApi, FrameData } from "./types"
 
-const maxElapsed = 40;
-let defaultElapsed = (1 / 60) * 1000;
-let useDefaultElapsed = true;
-let willRunNextFrame = false;
-let isProcessing = false;
+const maxElapsed = 40
+let defaultElapsed = (1 / 60) * 1000
+let useDefaultElapsed = true
+let willRunNextFrame = false
+let isProcessing = false
 
 const frame = {
-  delta: 0,
-  timestamp: 0
-};
+    delta: 0,
+    timestamp: 0,
+}
 
-const stepsOrder: StepId[] = ['read', 'update', 'render', 'postRender'];
+const stepsOrder: StepId[] = [
+    "read",
+    "update",
+    "preRender",
+    "render",
+    "postRender",
+]
 
-const setWillRunNextFrame = (willRun: boolean) => (willRunNextFrame = willRun);
+const setWillRunNextFrame = (willRun: boolean) => (willRunNextFrame = willRun)
 
-const steps = stepsOrder.reduce(
-  (acc, key) => {
-    acc[key] = createStep(setWillRunNextFrame);
-    return acc;
-  },
-  {} as SyncApi['steps']
-);
+const steps = stepsOrder.reduce((acc, key) => {
+    acc[key] = createStep(setWillRunNextFrame)
+    return acc
+}, {} as SyncApi["steps"])
 
-const sync = stepsOrder.reduce(
-  (acc, key) => {
-    const step = steps[key];
+const sync = stepsOrder.reduce((acc, key) => {
+    const step = steps[key]
     acc[key] = (process: Process, keepAlive = false, immediate = false) => {
-      if (!willRunNextFrame) startLoop();
-      step.schedule(process, keepAlive, immediate);
-      return process;
-    };
-    return acc;
-  },
-  {} as SyncApi['sync']
-);
+        if (!willRunNextFrame) startLoop()
+        step.schedule(process, keepAlive, immediate)
+        return process
+    }
+    return acc
+}, {} as SyncApi["sync"])
 
-const cancelSync = stepsOrder.reduce(
-  (acc, key) => {
-    acc[key] = steps[key].cancel;
-    return acc;
-  },
-  {} as SyncApi['cancelSync']
-);
+const cancelSync = stepsOrder.reduce((acc, key) => {
+    acc[key] = steps[key].cancel
+    return acc
+}, {} as SyncApi["cancelSync"])
 
-const processStep = (stepId: StepId) => steps[stepId].process(frame);
+const processStep = (stepId: StepId) => steps[stepId].process(frame)
 
 const processFrame = (timestamp: number) => {
-  willRunNextFrame = false;
+    willRunNextFrame = false
 
-  frame.delta = useDefaultElapsed
-    ? defaultElapsed
-    : Math.max(Math.min(timestamp - frame.timestamp, maxElapsed), 1);
+    frame.delta = useDefaultElapsed
+        ? defaultElapsed
+        : Math.max(Math.min(timestamp - frame.timestamp, maxElapsed), 1)
 
-  // Set this elapsed as default elapsed
-  // Maybe move this to a moving average for a more precise value
-  if (!useDefaultElapsed) defaultElapsed = frame.delta;
+    // Set this elapsed as default elapsed
+    // Maybe move this to a moving average for a more precise value
+    if (!useDefaultElapsed) defaultElapsed = frame.delta
 
-  frame.timestamp = timestamp;
+    frame.timestamp = timestamp
 
-  isProcessing = true;
-  stepsOrder.forEach(processStep);
-  isProcessing = false;
+    isProcessing = true
+    stepsOrder.forEach(processStep)
+    isProcessing = false
 
-  if (willRunNextFrame) {
-    useDefaultElapsed = false;
-    onNextFrame(processFrame);
-  }
-};
+    if (willRunNextFrame) {
+        useDefaultElapsed = false
+        onNextFrame(processFrame)
+    }
+}
 
 const startLoop = () => {
-  willRunNextFrame = true;
-  useDefaultElapsed = true;
+    willRunNextFrame = true
+    useDefaultElapsed = true
 
-  if (!isProcessing) onNextFrame(processFrame);
-};
+    if (!isProcessing) onNextFrame(processFrame)
+}
 
-const getFrameData = () => frame;
+const getFrameData = () => frame
 
-export default sync;
-export { cancelSync, getFrameData, FrameData, Process };
+export default sync
+export { cancelSync, getFrameData, FrameData, Process }
