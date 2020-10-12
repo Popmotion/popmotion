c3e353fd139626bda32f85d48b7cbcccc9303b77
InventingWithMonster <mattgperry@gmail.com>
2020-10-12 10:42:40 +0200

    Updating spring test

--- packages/popmotion/src/animations/generators/__tests__/spring.test.ts
+++ packages/popmotion/src/animations/generators/__tests__/spring.test.ts
@@ -19,11 +19,11 @@ describe("spring", () => {
             )
         ).toEqual([100, 1343, 873, 1046, 984, 1005, 998, 1001, 1000])
     })
+
     test("Velocity passed to underdamped spring", () => {
         const settings = {
             from: 100,
             to: 1000,
-            velocity: 1000,
             stiffness: 300,
             restSpeed: 10,
             restDelta: 0.5,
@@ -37,6 +37,7 @@ describe("spring", () => {
 
         expect(noVelocity).not.toEqual(velocity)
     })
+
     test("Critically damped spring", () => {
         expect(
             animateSync(
@@ -52,6 +53,26 @@ describe("spring", () => {
             )
         ).toEqual([100, 635, 918, 984, 997, 1000])
     })
+
+    test("Velocity passed to critically spring", () => {
+        const settings = {
+            from: 100,
+            to: 1000,
+            stiffness: 100,
+            damping: 20,
+            restSpeed: 10,
+            restDelta: 0.5,
+        }
+
+        const noVelocity = animateSync(spring(settings), 200)
+        const velocity = animateSync(
+            spring({ ...settings, velocity: 1000 }),
+            200
+        )
+
+        expect(noVelocity).not.toEqual(velocity)
+    })
+
     test("Overdamped spring", () => {
         expect(
             animateSync(
@@ -96,5 +117,24 @@ describe("spring", () => {
                 200
             )
         ).toEqual([100, 1000])
+    })
+
+    test("Velocity passed to overdamped spring", () => {
+        const settings = {
+            from: 100,
+            to: 1000,
+            stiffness: 300,
+            damping: 100,
+            restSpeed: 10,
+            restDelta: 0.5,
+        }
+
+        const noVelocity = animateSync(spring(settings), 200)
+        const velocity = animateSync(
+            spring({ ...settings, velocity: 1000 }),
+            200
+        )
+
+        expect(noVelocity).not.toEqual(velocity)
     })
 })
