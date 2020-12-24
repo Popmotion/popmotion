c27f962913154b80614bec550d4d90b4eed18f39
InventingWithMonster <mattgperry@gmail.com>
2020-12-24 12:38:47 +0000

    Adding tests for relative projection

--- packages/projection/cypress/integration/index.spec.js
+++ packages/projection/cypress/integration/index.spec.js
@@ -20,5 +20,35 @@ describe("Layout projection", () => {
                 expect(right).to.equal(400)
                 expect(bottom).to.equal(500)
             })
+            .get("#absolute-child")
+            .should(($test) => {
+                const element = $test[0]
+                const {
+                    left,
+                    top,
+                    bottom,
+                    right,
+                } = element.getBoundingClientRect()
+
+                expect(left).to.equal(200)
+                expect(top).to.equal(300)
+                expect(right).to.equal(800)
+                expect(bottom).to.equal(330)
+            })
+            .get("#relative-child")
+            .should(($test) => {
+                const element = $test[0]
+                const {
+                    left,
+                    top,
+                    bottom,
+                    right,
+                } = element.getBoundingClientRect()
+
+                expect(left).to.equal(210)
+                expect(top).to.equal(210)
+                expect(right).to.equal(310)
+                expect(bottom).to.equal(310)
+            })
     })
 })
