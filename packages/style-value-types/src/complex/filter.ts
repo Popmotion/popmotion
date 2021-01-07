51e7de72d152a4328a0edefc56d32e1d3b2f1c01
InventingWithMonster <mattgperry@gmail.com>
2021-01-07 16:21:35 +0100

    Fixing tests

--- packages/style-value-types/src/complex/filter.ts
+++ packages/style-value-types/src/complex/filter.ts
@@ -9,16 +9,16 @@ const maxDefaults = new Set(['brightness', 'contrast', 'saturate', 'opacity']);
 function applyDefaultFilter(v: string) {
   let [name, value] = v.slice(0, -1).split('(');
 
-  if (name === 'drop-shadow') {
-    return v;
-  } else {
-    const number = value.match(floatRegex)?.[0] ?? '0';
-    const unit = value.replace(number, '');
-    let defaultValue = maxDefaults.has(name) ? 1 : 0;
-    if (number === value) defaultValue *= 100;
+  if (name === 'drop-shadow') return v;
 
-    return `${name}(${defaultValue}${unit})`;
-  }
+  const [number] = value.match(floatRegex) || [];
+  if (!number) return v;
+
+  const unit = value.replace(number, '');
+  let defaultValue = maxDefaults.has(name) ? 1 : 0;
+  if (number !== value) defaultValue *= 100;
+
+  return `${name}(${defaultValue}${unit})`;
 }
 
 const functionRegex = /([a-z-]*)\(.*?\)/g;
@@ -26,7 +26,7 @@ const functionRegex = /([a-z-]*)\(.*?\)/g;
 export const filter = {
   ...complex,
   getAnimatableNone: (v: string) => {
-    const functions = v.match(functionRegex) || [];
-    return functions.map(applyDefaultFilter).join(' ');
+    const functions = v.match(functionRegex);
+    return functions ? functions.map(applyDefaultFilter).join(' ') : v;
   },
 };
