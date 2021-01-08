aeb4ba2d584c1fe21658c8eb635df25aed4ff0f7
InventingWithMonster <mattgperry@gmail.com>
2021-01-08 11:16:58 +0100

    Fixing types

--- packages/style-value-types/src/color/utils.ts
+++ packages/style-value-types/src/color/utils.ts
@@ -5,10 +5,10 @@ import { floatRegex, isString, singleColorRegex } from '../utils';
  * Returns true if the provided string is a color, ie rgba(0,0,0,0) or #000,
  * but false if a number or multiple colors
  */
-export const isColorString = (type: string, testProp: string) => (v: any) => {
+export const isColorString = (type: string, testProp?: string) => (v: any) => {
   return (
     (isString(v) && singleColorRegex.test(v) && v.startsWith(type)) ||
-    v.hasOwnProperty(testProp)
+    (testProp && v.hasOwnProperty(testProp))
   );
 };
 
